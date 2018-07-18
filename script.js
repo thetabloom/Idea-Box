32691273a176ab2bd7ca4633579665a01623713b
Sara Ebbers <johndoe@example.com>
2018-07-18 06:12:30 -0600

    Add clear Input, refactor to function saveIdea, card recreated in HTML for styling

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
+// global variables 
+
 var inputTitle = document.querySelector('.input-title');
 var inputBody = document.querySelector('.input-body');
 var saveButton = document.querySelector('.save-button');
 var searchInput = document.querySelector('.search');
 var cardsLiveHere = document.querySelector('.cards-live-here');
+var ideasArray = JSON.parse(localStorage.getItem('ideasArray')) || [];
 
+// event listeners
 
-saveButton.addEventListener('click', function(event){
-  event.preventDefault()
+saveButton.addEventListener('click', saveIdea);
+
+// constructor functions
+
+function Idea(id, title, body) {
+  this.id = id;
+  this.title = title;
+  this.body = body;
+  this.quality = 'swill';
+}
+
+// functions
+
+function saveIdea(event) {
+  event.preventDefault();
   var title = inputTitle.value;
   var body = inputBody.value;
   var newLi = document.createElement('li');
+  var newIdea = new Idea(Date.now(), title, body);
+  ideasArray.push(newIdea);
+  localStorage.setItem('ideasArray', JSON.stringify(ideasArray));
   newLi.innerHTML = `<div class="title-line-parent-container">
         <h3>${title}</h3>
         <button class="delete-X">X</button>
@@ -22,9 +42,6 @@ saveButton.addEventListener('click', function(event){
         <br>
       </div>`
   cardsLiveHere.prepend(newLi);
+  clearInputs();
+};
 
-   
-
-
-
-});
